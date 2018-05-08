2ab092c09eeb7a26c97d3151925d1855fe05f831
Nils Friess <nils.friess@gmail.com>
2018-05-08 08:34:25 +0200

    another deployment test

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ if [ "$TRAVIS_BRANCH" == "production" ]; then
   mix edeliver build release --branch=production --verbose
   mix edeliver deploy release to production --verbose
 elif [ "$TRAVIS_BRANCH" == "development" ]; then
+  git checkout -B development
   mix edeliver build release --branch=development --verbose
   mix edeliver deploy release to staging --verbose
   mix edeliver start staging --verbose
